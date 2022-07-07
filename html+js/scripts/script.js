e53817ac5169add7b4927da71aaae6f829b3bf69
daklyan <daklyan@protonmail.com>
2022-07-07 10:59:37 +0200

    Beginning of PKCE implem

--- html+js/scripts/script.js
+++ html+js/scripts/script.js
@@ -1,6 +1,9 @@
-const client_id = 'bc588e09eb5e4b3a8e01068e211953e9'
-const redirect_uri = 'http://falconia.fr/spotython/index.html'
-const api_url = 'https://api.spotify.com/'
+const CLIENT_ID = 'bc588e09eb5e4b3a8e01068e211953e9'
+const REDIRECT_URI = 'http://falconia.fr/spotython/index.html'
+const API_URL = 'https://api.spotify.com'
+const SPOTIFY_AUTH_URL = 'https://accounts.spotify.com/'
+
+// Cookies
 
 function setCookie(cookieName, value, expiration){
     let expireDate = new Date();
@@ -8,11 +11,12 @@ function setCookie(cookieName, value, expiration){
     let cookieValue=escape(value) + ((expiration==null) ? "" : "expires="+expireDate.toUTCString());
     document.cookie = cookieName + "=" + cookieValue;
 }
+
 function getCookie(cookieName){
     let cookieValue = document.cookie;
     let cookieStart = cookieValue.indexOf("" + cookieName + "=");
     if (cookieStart == -1){
-        cookieStart= c_valeur.indexOf(cookieName + "=");
+        cookieStart= cookieValue.indexOf(cookieName + "=");
     }
     if (cookieStart == -1){
         cookieValue = null;
@@ -27,6 +31,47 @@ function getCookie(cookieName){
     return cookieValue;
 }
 
+// Code verifier generator - PKCE
+
+function dec2hex(dec) {
+  return ("0" + dec.toString(16)).substr(-2);
+}
+
+function generateCodeVerifier() {
+  var array = new Uint32Array(56 / 2);
+  window.crypto.getRandomValues(array);
+  return Array.from(array, dec2hex).join("");
+}
+
+// Code challenge generator - PKCE
+
+function sha256(plain) {
+  // returns promise ArrayBuffer
+  const encoder = new TextEncoder();
+  const data = encoder.encode(plain);
+  return window.crypto.subtle.digest("SHA-256", data);
+}
+
+function base64urlencode(a) {
+  var str = "";
+  var bytes = new Uint8Array(a);
+  var len = bytes.byteLength;
+  for (var i = 0; i < len; i++) {
+    str += String.fromCharCode(bytes[i]);
+  }
+  return btoa(str)
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_")
+    .replace(/=+$/, "");
+}
+
+async function generateCodeChallengeFromVerifier(v) {
+  var hashed = await sha256(v);
+  var base64encoded = base64urlencode(hashed);
+  return base64encoded;
+}
+
+// State generator
 function randomString(length) {
     let result           = '';
     let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -43,8 +88,14 @@ let app = new Vue({
     data: {
         logged: false,
         spotifyCode: null,
+		state: null,
+		codeChallenge: null,
+		codeChallengeVerifier: null,
     },
     async beforeCreate() {
+	    this.state = randomString(16)
+		this.codeChallengeVerifier = generateCodeVerifier()
+		this.codeChallenge = generateCodeChallengeFromVerifier(this.codeChallengerVerifier)
     },
     // Start when page is loaded
     mounted(){
@@ -66,19 +117,40 @@ let app = new Vue({
     computed: {
     },
     methods: {
-        async login(){
+        // Gets code from spotify needed to get access token
+		login(){
             if(!this.logged) {
-                let state = randomString(16);
-                setCookie('spotifyState', state, 60)
+                setCookie('spotifyState', this.state, 60)
                 let scope = 'user-read-private user-read-email';
-                window.location.replace('https://accounts.spotify.com/authorize?' +
+                window.location.replace(SPOTIFY_AUTH_URL + 'authorize?' +
                     'response_type=code' +
-                    '&client_id=' + client_id.toString() +
+                    '&CLIENT_ID=' + CLIENT_ID.toString() +
                     '&scope=' + scope.toString() +
-                    '&redirect_uri=' + redirect_uri.toString() +
-                    '&state=' + state.toString()
+                    '&REDIRECT_URI=' + REDIRECT_URI.toString() +
+                    '&state=' + this.state.toString() +
+					'&code_challenge_method=S256' + 
+					'&code_challenge=' + this.codeChallenge
                 );
             }
-        }
-    }
+			getAccessToken()
+        },
+		// Get access token needed to get data from spotify
+		async getAccessToken(){
+		    if(this.code){
+				let content = {
+					"grant_type": "authorization_code",
+					"code": this.code,
+					"redirect_uri": REDIRECT_URI,
+					"client_id": CLIENT_ID,
+					"code_verifier": this.codeChallengeVerifier,
+				}
+	        	resp = await fetch(`${API_URL}/token`, {
+                method: 'POST',
+                headers: {'Authorization': 'Basic ' + (new Buffer(CLIENT_ID + ':' + ''))},
+                body: JSON.stringify(content)
+                }).then(response => response.json())
+				console.log(resp)
+			}
+		},
+	}
 })
